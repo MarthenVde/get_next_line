98e59879f3ebe0ae3e3bb0bb25a25f7cc438c35c
Marthen van der Eems <marvan-d@c3r6s9.wethinkcode.co.za>
2019-06-10 15:33:22 +0200

    added header file and get_next_line.c (does nothing yet)

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvan-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 10:51:04 by marvan-d          #+#    #+#             */
+/*   Updated: 2019/06/10 10:51:05 by marvan-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+int	get_next_line(const int fd, char **line);
+#endif
