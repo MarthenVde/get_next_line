a58784dbec84cee10e39b1007939c10647684654
Marthen van der Eems <marvan-d@c3r7s6.wethinkcode.co.za>
2019-06-21 12:47:09 +0200

    fixed libft issue

---
+++ get_next_line.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvan-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 10:49:45 by marvan-d          #+#    #+#             */
+/*   Updated: 2019/06/10 10:49:47 by marvan-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_realloc_str(char *str, char *new)
+{
+	char *tmp;
+
+	tmp = str;
+	str = new;
+	free(tmp);
+	return (new);
+}
+
+char	*read_to_str(int fd, char *str)
+{
+	char	buff[BUFF_SIZE + 1];
+	ssize_t	b_read;
+
+	ft_memset(buff, 0, BUFF_SIZE);
+	if (read(fd, NULL, 0) < 0 || fd < 0 || BUFF_SIZE < 1)
+		return (NULL);
+	if (!str)
+		str = ft_strnew(1);
+	while (!(ft_strchr(str, '\n')))
+	{
+		if ((b_read = read(fd, buff, BUFF_SIZE)) < 0)
+			return (NULL);
+		buff[b_read] = '\0';
+		str = ft_realloc_str(str, ft_strjoin(str, buff));
+		if (str[0] == '\0' || b_read == 0)
+			break ;
+	}
+	return (str);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char *str;
+	char		*adr_lf;
+	int			ret;
+
+	if (!line || !(str = read_to_str(fd, str)))
+		return (-1);
+	adr_lf = ft_strchr(str, '\n');
+	if (adr_lf != NULL)
+	{
+		if (!(*line = (ft_strndup(str, adr_lf - str))))
+			return (-1);
+		str = ft_realloc_str(str, ft_strdup(adr_lf + 1));
+		return (1);
+	}
+	if (str[0] == '\0')
+		ret = 0;
+	else if (!(*line = ft_strdup(str)))
+		ret = -1;
+	else
+		ret = 1;
+	ft_strdel(&str);
+	return (ret);
+}
